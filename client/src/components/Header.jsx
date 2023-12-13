0836b3f00feb4388b817b74cc6a5b951b72004de
Kenna <kenna.hwa@gmail.com>
2023-12-14 01:25:28 +0900

    Feat: header 생성

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Complete  = () => {
+
+	return (<header>
+	<div>logo</div>
+	<div>
+		<ul>
+			<li>공유</li>
+			<li>메뉴</li>
+		</ul>
+	</div>
+	</header>
+	);
+}
+
+export default Complete;
