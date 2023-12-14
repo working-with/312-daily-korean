490787993718497f1d956ac0e9b64d90f3871b8f
Kenna <kenna.hwa@gmail.com>
2023-12-14 21:42:47 +0900

    feat: header 마크업 생성

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 import hamburgerIcon from '../assets/hamburger_icon.png';
 import shareIcon from '../assets/share_icon.png';
+import theme from '../style/theme'
 
 const Wrapper = styled.header`
 height: 62px;
-border: 1px solid red;
+background: ${theme.colors.yellow2};
 display: flex;
 flex-flow: row nowrap;
 align-items: center;
@@ -58,13 +59,32 @@ text-indent: -9999px;
 cursor: pointer;
 `
 
-const Complete  = () => {
+	const handleShare = async () => {
+		if (navigator.share) {
+			try {
+				await navigator.share({
+					title: 'Title to share', // 공유될 제목
+					text: 'Check out this URL', // 공유될 텍스트
+					url: 'https://localhost:3000/' // 공유될 URL
+				});
+				console.log('공유 완료!');
+			} catch (error) {
+				console.error('공유 실패:', error);
+			}
+		} else {
+			console.log('Web Share API를 지원하지 않는 브라우저입니다.');
+		}
+	}
 
+const Header  = () => {
+
+
+	
 	return (<Wrapper>
-	<Logo className='header_logo'>logo</Logo>
+	<Logo className='header_logo'><a href="#">logo</a></Logo>
 	<Menu className='header_menu'>
 		<MenuUl>
-			<MenuShare><MenuShareBtn>공유</MenuShareBtn></MenuShare>
+			<MenuShare><MenuShareBtn onClick={handleShare}>공유</MenuShareBtn></MenuShare>
 			<MenuDraw><MenuDrawBtn>메뉴</MenuDrawBtn></MenuDraw>
 		</MenuUl>
 	</Menu>
@@ -72,4 +92,4 @@ const Complete  = () => {
 	);
 }			
 
-export default Complete;
+export default Header;
