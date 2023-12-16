23561ce69002fe70abd576ac970de34fc8cf8811
SO HUI <haejuuun925@gmail.com>
2023-12-16 14:15:11 +0900

    feat: loading page 구현

--- client/src/components/Loading.jsx
+++ client/src/components/Loading.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import Generating from '../assets/generating.png';
+import styled from 'styled-components';
+import theme from './../style/theme';
+import LoadingBg from "../assets/loading_background.png";
 
 const Loading  = () => {
 
-	return (<>
-	로딩 페이지 입니다.
-	</>
+	return (
+		<Wrapper>
+			<img alt='로딩 이미지'/>
+			<CreateIngTitle />
+		</Wrapper>
 	);
 }
 
 export default Loading;
+
+const Wrapper = styled.div`
+  background: url(${LoadingBg}) no-repeat center;
+  background-size: cover;
+	height: 704px;
+	padding-top: 50%;
+	text-align: center;
+`;
+
+const CreateIngTitle = styled.h2`
+	width: 222px;
+	height: 23px;
+  background: url(${Generating}) no-repeat center;
+  background-size: contain;
+  text-indent: -9999px;
+	margin: auto;
+	margin-top: 20px;
+`;
