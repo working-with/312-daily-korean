fd1b6eb20fc35ba16aca4d00b6bb77729eb1a576
Kenna <kenna.hwa@gmail.com>
2023-12-16 14:07:21 +0900

    fix: delete basename

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ position: relative;
 
 const Router = () => {
   return (
-    <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Routes>
           <Route path="/" element={<Splash />} />
           <Route path="/onboarding" element={<OnBoarding />} />
